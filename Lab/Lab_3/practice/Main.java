90ded034cc3a7c009abc55d04093224b5cfa48ca
Phan-Ngoc-Hanh-Nhi <155942839+nhiph4303@users.noreply.github.com>
2025-01-22 08:45:52 +0700

    Update Main.java

--- Lab/Lab_3/practice/Main.java
+++ Lab/Lab_3/practice/Main.java
@@ -1,12 +1,26 @@
+import java.io.*;
 
 public class Main {
     public static final String TASK_FILE = "schedule.txt";
 
     public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(Main.TASK_FILE));
+            BufferedReader rd = new BufferedReader(new FileReader(Main.TASK_FILE));
+            String line;
+            line = rd.readLine();
+            while (line!=null){
+                String [] components = line.split(",");
+                System.out.println(components[0]);
+                System.out.println(components[1]);
+                System.out.println(components[2]);
+                System.out.println("------------------------------------");
+                line = rd.readLine();
+            }
         } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+            // System.out.println("File not found " + e.getMessage());
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
